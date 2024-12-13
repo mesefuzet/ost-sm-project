8862dfc0a0ae822c89e8884a0c6fb222a19b4d50
corvinus-ijklsh <emese.vastag@stud.uni-corvinus.hu>
2024-12-13 14:20:25 +0100

    Establishing second streaming process (Streaming KMeans)

--- scripts/1_Emese_spark_streaming_streaming_KMeans.py
+++ scripts/1_Emese_spark_streaming_streaming_KMeans.py
@@ -1,18 +1,17 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, expr
-from pyspark.sql.types import StringType, FloatType, IntegerType
+from pyspark.sql.functions import col, expr
 import os
 from sqlalchemy import create_engine, Column, Integer, Float, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 import pandas as pd
-import time
+from river.cluster import KMeans
+import numpy as np
 
 if os.path.exists("streaming_kmeans_result.db"):
     os.remove("streaming_kmeans_result.db")
     print("[DEBUG] Database file deleted.")
 else:
     print("[DEBUG] No existing database file to delete.")
-
 
 # SQLAlchemy DatabaseManager class
 Base = declarative_base()
@@ -50,7 +49,7 @@ class DatabaseManager:
             session.commit()
         except Exception as e:
             session.rollback()
-            print(f"Error inserting data: {e}")
+            print(f"[ERROR] Error inserting data: {e}")
         finally:
             session.close()
 
@@ -67,22 +66,20 @@ topic_name = "hai-dataset"
 
 # Database Configuration
 DATABASE_URL = "sqlite:///streaming_kmeans_result.db"
-#train_schema = {"timestamp": String, "P1_FCV01D": Float, "P1_FCV01Z": Float, "P1_FCV03D": Float, "x1003_24_SUM_OUT": Float, "data_type": String}
 train_schema = {
     "timestamp": String,
     "P1_FCV01D": Float,
     "P1_FCV01Z": Float,
     "P1_FCV03D": Float,
+    "P1_FT01": Float,
     "x1003_24_SUM_OUT": Float,
     "data_type": String,
-    "drift_detected": Integer,  
+    "cluster": Integer
 }
 test_schema = {**train_schema, "attack_label": Integer}
 
-#Database manager configuration
 train_db = DatabaseManager(DATABASE_URL, "train", train_schema)
 test_db = DatabaseManager(DATABASE_URL, "test", test_schema)
-
 
 # Read from Kafka
 kafka_stream = spark.readStream \
@@ -93,95 +90,96 @@ kafka_stream = spark.readStream \
     .option("failOnDataLoss", "false") \
     .load()
 
-#I just put this here for debug, it logs into a json what Spark gets from Kafka, can be commented out later:
-# kafka_stream.selectExpr("CAST(value AS STRING)").writeStream \
-#     .outputMode("append") \
-#     .format("json") \
-#     .option("path", "debug/raw_kafka_output") \
-#     .option("checkpointLocation", "debug/raw_kafka_checkpoint") \
-#     .start()
-
 raw_stream = kafka_stream.selectExpr("CAST(value AS STRING) as raw_data")
-
 
 parsed_stream = raw_stream \
     .withColumn("timestamp", expr(r"regexp_extract(raw_data, '\"timestamp\":\\s*\"(.*?)\"', 1)").cast("string")) \
-    .withColumn("P1_FCV01D", expr(r"regexp_extract(raw_data, '\"P1_FCV01D\":\\s*([0-9.]+)', 1)")) \
-    .withColumn("P1_FCV01Z", expr(r"regexp_extract(raw_data, '\"P1_FCV01Z\":\\s*([0-9.]+)', 1)")) \
-    .withColumn("P1_FCV03D", expr(r"regexp_extract(raw_data, '\"P1_FCV03D\":\\s*([0-9.]+)', 1)")) \
+    .withColumn("P1_FCV01D", expr(r"regexp_extract(raw_data, '\"P1_FCV01D\":\\s*([0-9.]+)', 1)").cast("double")) \
+    .withColumn("P1_FCV01Z", expr(r"regexp_extract(raw_data, '\"P1_FCV01Z\":\\s*([0-9.]+)', 1)").cast("double")) \
+    .withColumn("P1_FCV03D", expr(r"regexp_extract(raw_data, '\"P1_FCV03D\":\\s*([0-9.]+)', 1)").cast("double")) \
+    .withColumn("P1_FT01", expr(r"regexp_extract(raw_data, '\"P1_FCV03D\":\\s*([0-9.]+)', 1)").cast("double")) \
     .withColumn("x1003_24_SUM_OUT", expr(r"regexp_extract(raw_data, '\"x1003_24_SUM_OUT\":\\s*([0-9.]+)', 1)").cast("double")) \
     .withColumn("data_type", expr(r"regexp_extract(raw_data, '\"data_type\":\\s*\"(.*?)\"', 1)").cast("string")) \
     .withColumn("attack_label", expr(r"regexp_extract(raw_data, '\"attack_label\":\\s*([0-9]+)', 1)").cast("int"))
 
-parsed_stream = parsed_stream \
-    .withColumn("P1_FCV01D", col("P1_FCV01D").cast("double")) \
-    .withColumn("P1_FCV01Z", col("P1_FCV01Z").cast("double")) \
-    .withColumn("P1_FCV03D", col("P1_FCV03D").cast("double")) \
-    .withColumn("P1_FCV03D", col("x1003_24_SUM_OUT").cast("double"))
-
-#Filter out rows that are completely blank or contain only zeros in numeric columns
+# Filter rows with valid data
 parsed_stream = parsed_stream.filter(
     (col("timestamp").isNotNull()) &
-    ((col("P1_FCV01D") != 0) | (col("P1_FCV01Z") != 0) | (col("P1_FCV03D") != 0) | (col("x1003_24_SUM_OUT") != 0))
+    ((col("P1_FCV01D") != 0) | (col("P1_FCV01Z") != 0) | (col("P1_FCV03D") != 0) | (col("P1_FT01") != 0) | (col("x1003_24_SUM_OUT") != 0))
 )
 
-#check the schema
-#parsed_stream.printSchema()
-
-#train-test separation
+# Separate train and test streams
 train_stream = parsed_stream.filter(col("data_type") == "train") \
-    .select("timestamp", "P1_FCV01D", "P1_FCV01Z", "P1_FCV03D", "x1003_24_SUM_OUT", "data_type")
-
-train_stream.printSchema()
+    .select("timestamp", "P1_FCV01D", "P1_FCV01Z", "P1_FCV03D", "P1_FT01", "x1003_24_SUM_OUT", "data_type")
 
 test_stream = parsed_stream.filter(col("data_type") == "test") \
-    .select("timestamp", "P1_FCV01D", "P1_FCV01Z", "P1_FCV03D", "x1003_24_SUM_OUT", "data_type", "attack_label")
+    .select("timestamp", "P1_FCV01D", "P1_FCV01Z", "P1_FCV03D", "P1_FT01", "x1003_24_SUM_OUT", "data_type", "attack_label")
 
-# Select specific columns
-# selected_columns = parsed_stream.select(
-#     col("timestamp"),
-#     col("P1_FCV01D"),
-#     col("P1_FCV01Z"),
-#     col("P1_FCV03D"),
-#     col("x1003_24_SUM_OUT"),
-#     col("data_type"),
-#     col("attack_label")
-    
-# )
+# Initialize River's KMeans model
+kmeans_model = KMeans(n_clusters=3, halflife=0.5)
 
+#☺train Kmeans
+def process_train_batch(df, epoch_id):
+    global kmeans_model  
+    print(f"[DEBUG] Invoked process_train_batch for Epoch: {epoch_id}")
+    try:
+        batch_df = df.toPandas()
 
-# Debugging: Save parsed output to a file ---> when something is bad, we can comment this out and have a look at the log files in debug/
-# selected_columns.writeStream \
-#     .outputMode("append") \
-#     .format("json") \
-#     .option("path", "debug/parsed_output") \
-#     .option("checkpointLocation", "debug/parsed_checkpoint") \
-#     .start()
+        if batch_df.empty:
+            print(f"[INFO] Epoch {epoch_id}: Empty training batch, skipping.")
+            return
 
-# train_stream.writeStream \
-#     .outputMode("append") \
-#     .format("json") \
-#     .option("path", "debug/train_output") \
-#     .option("checkpointLocation", "debug/train_checkpoint") \
-#     .start()
+        print(f"[DEBUG] Epoch {epoch_id}: Training batch size: {len(batch_df)}")
+        print(batch_df.head())  
 
-# test_stream.writeStream \
-#     .outputMode("append") \
-#     .format("json") \
-#     .option("path", "debug/test_output") \
-#     .option("checkpointLocation", "debug/test_checkpoint") \
-#     .start()
+        #train
+        features = batch_df[["P1_FCV01D", "P1_FCV01Z", "P1_FCV03D", "P1_FT01", "x1003_24_SUM_OUT"]].to_numpy()
+        for row in features:
+            kmeans_model.learn_one(dict(enumerate(row)))  # Update model without reassigning
 
+        #predict
+        batch_df["cluster"] = [kmeans_model.predict_one(dict(enumerate(row))) for row in features]
 
-# ===============================================
-### EMESE 2 STREAMING PROCESSES ####
-# 2. STREAMIN KMEANS method
+        #wrtie back to database
+        train_db.bulk_insert(batch_df)
+        print(f"[DEBUG] Epoch {epoch_id}: Train results written to database.")
+    except Exception as e:
+        print(f"[ERROR] Epoch {epoch_id}: Error processing train batch: {e}")
 
-print("2. PROCESS: STREAMING K-MEANS")
-time.sleep(1)
+#function for testing
+def process_test_batch(df, epoch_id):
+    global kmeans_model  #--> we need global instance so code goes good
+    print(f"[DEBUG] Invoked process_test_batch for Epoch: {epoch_id}")
+    try:
+        batch_df = df.toPandas()
 
-#WRITE THE STREAMING K-MEANS HERE WITH WRITING BACK THE RESULTS TO THE DATASET
+        if batch_df.empty:
+            print(f"[INFO] Epoch {epoch_id}: Empty testing batch, skipping.")
+            return
 
+        print(f"[DEBUG] Epoch {epoch_id}: Testing batch size: {len(batch_df)}")
+        print(batch_df.head())  
+
+        #prediction
+        features = batch_df[["P1_FCV01D", "P1_FCV01Z", "P1_FCV03D", "x1003_24_SUM_OUT"]].to_numpy()
+        batch_df["cluster"] = [kmeans_model.predict_one(dict(enumerate(row))) for row in features]
+
+        # Write test results to database
+        test_db.bulk_insert(batch_df)
+        print(f"[DEBUG] Epoch {epoch_id}: Test results written to database.")
+    except Exception as e:
+        print(f"[ERROR] Epoch {epoch_id}: Error processing test batch: {e}")
+
+#application of functions
+train_stream.writeStream \
+    .foreachBatch(lambda df, epoch_id: process_train_batch(df, epoch_id)) \
+    .option("checkpointLocation", "checkpoints/train_kmeans_checkpoint") \
+    .start()
+
+test_stream.writeStream \
+    .foreachBatch(lambda df, epoch_id: process_test_batch(df, epoch_id)) \
+    .option("checkpointLocation", "checkpoints/test_kmeans_checkpoint") \
+    .start()
 
 try:
     spark.streams.awaitAnyTermination()
