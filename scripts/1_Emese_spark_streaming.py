a41f5033176f3b489e633f65dea1a645b29da45d
corvinus-ijklsh <emese.vastag@stud.uni-corvinus.hu>
2024-12-13 12:04:02 +0100

    Code for ADWIN analysis, untrack corresponding Spark checkpoints

--- scripts/1_Emese_spark_streaming.py
+++ scripts/1_Emese_spark_streaming.py
@@ -1,22 +1,15 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, mean, stddev, count, isnan, when, lit, split, regexp_replace, expr
-from pyspark.sql.types import StructType, StringType, DoubleType
-import json
+from pyspark.sql.functions import col, count, expr
+from pyspark.sql.types import StringType, FloatType, IntegerType
 import os
-from pyspark.sql.functions import udf
-from pyspark.sql.functions import from_json
-from pyspark.ml.feature import VectorAssembler
-from pyspark.sql.functions import min, max
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType
-from pyspark.sql.functions import regexp_replace
-from pyspark.sql import functions as F
-import time
 from sqlalchemy import create_engine, Column, Integer, Float, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 import pandas as pd
+from river.drift import ADWIN
+import time
 
-if os.path.exists("hai_train_test.db"):
-    os.remove("hai_train_test.db")
+if os.path.exists("adwin_results.db"):
+    os.remove("adwin_results.db")
     print("[DEBUG] Database file deleted.")
 else:
     print("[DEBUG] No existing database file to delete.")
@@ -74,8 +67,17 @@ kafka_broker = "localhost:9092"
 topic_name = "hai-dataset"
 
 # Database Configuration
-DATABASE_URL = "sqlite:///hai_train_test.db"
-train_schema = {"timestamp": String, "P1_FCV01D": Float, "P1_FCV01Z": Float, "P1_FCV03D": Float, "x1003_24_SUM_OUT": Float, "data_type": String}
+DATABASE_URL = "sqlite:///adwin_results.db"
+#train_schema = {"timestamp": String, "P1_FCV01D": Float, "P1_FCV01Z": Float, "P1_FCV03D": Float, "x1003_24_SUM_OUT": Float, "data_type": String}
+train_schema = {
+    "timestamp": String,
+    "P1_FCV01D": Float,
+    "P1_FCV01Z": Float,
+    "P1_FCV03D": Float,
+    "x1003_24_SUM_OUT": Float,
+    "data_type": String,
+    "drift_detected": Integer,  
+}
 test_schema = {**train_schema, "attack_label": Integer}
 
 #Database manager configuration
@@ -131,20 +133,22 @@ parsed_stream = parsed_stream.filter(
 train_stream = parsed_stream.filter(col("data_type") == "train") \
     .select("timestamp", "P1_FCV01D", "P1_FCV01Z", "P1_FCV03D", "x1003_24_SUM_OUT", "data_type")
 
+train_stream.printSchema()
+
 test_stream = parsed_stream.filter(col("data_type") == "test") \
     .select("timestamp", "P1_FCV01D", "P1_FCV01Z", "P1_FCV03D", "x1003_24_SUM_OUT", "data_type", "attack_label")
 
-# Select specific columns (adjust indices as per your schema)
-selected_columns = parsed_stream.select(
-    col("timestamp"),
-    col("P1_FCV01D"),
-    col("P1_FCV01Z"),
-    col("P1_FCV03D"),
-    col("x1003_24_SUM_OUT"),
-    col("data_type"),
-    col("attack_label")
+# Select specific columns
+# selected_columns = parsed_stream.select(
+#     col("timestamp"),
+#     col("P1_FCV01D"),
+#     col("P1_FCV01Z"),
+#     col("P1_FCV03D"),
+#     col("x1003_24_SUM_OUT"),
+#     col("data_type"),
+#     col("attack_label")
     
-)
+# )
 
 
 # Debugging: Save parsed output to a file ---> when something is bad, we can comment this out and have a look at the log files in debug/
@@ -168,118 +172,97 @@ selected_columns = parsed_stream.select(
 #     .option("path", "debug/test_output") \
 #     .option("checkpointLocation", "debug/test_checkpoint") \
 #     .start()
-print("---------TRAIN DATA----------------")
-time.sleep(1)
-train_stream.writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .option("truncate", "false") \
-    .start()
 
-print("---------TEST DATA----------------")
-time.sleep(1)
-test_stream.writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .option("truncate", "false") \
-    .start()
-
-print("Train row count:")
-train_stream.select(count("*")).writeStream \
-    .outputMode("complete") \
-    .format("console") \
-    .start()
-
-print("Test row count:")
-test_stream.select(count("*")).writeStream \
-    .outputMode("complete") \
-    .format("console") \
-    .start()
 
 # ===============================================
 ### EMESE 2 STREAMING PROCESSES ####
-# Data Exploration Tasks
-# 1. Detect Missing Values & Basic Statistics
+# 1. ADWIN method
 
-print("1. PROCESS: DETECTION OF REMAINING MISSING VALUES & MAIN STATISTICS")
+print("1. PROCESS: ADWIN")
 time.sleep(1)
-missing_counts_train = train_stream.select(
-    [(count(when(isnan(c) | col(c).isNull(), c))).alias(c) for c in train_stream.columns]
-)
 
-missing_counts_test = test_stream.select(
-    [(count(when(isnan(c) | col(c).isNull(), c))).alias(c) for c in test_stream.columns]
-)
-
-# missing_counts_train.writeStream \
-#     .outputMode("complete") \
-#     .format("console") \
-#     .option("truncate", "false") \
-#     .start()
-
-# missing_counts_test.writeStream \
-#     .outputMode("complete") \
-#     .format("console") \
-#     .option("truncate", "false") \
-#     .start()
-
-
-
-# Process 2: Statistics Calculation
-train_stats = train_stream.select(
-    mean(col("x1003_24_SUM_OUT")).alias("train_mean"),
-    stddev(col("x1003_24_SUM_OUT")).alias("train_stddev")
-)
-
-test_stats = test_stream.select(
-    mean(col("x1003_24_SUM_OUT")).alias("test_mean"),
-    stddev(col("x1003_24_SUM_OUT")).alias("test_stddev")
-)
-
-# train_stats.writeStream \
-#     .outputMode("complete") \
-#     .format("console") \
-#     .option("truncate", "false") \
-#     .start()
-
-# test_stats.writeStream \
-#     .outputMode("complete") \
-#     .format("console") \
-#     .option("truncate", "false") \
-#     .start()
-
-def write_to_database(df, epoch_id, db_manager):
-    """Write micro-batch to database."""
+adwin = ADWIN()
+def process_adwin_batch(df, epoch_id, is_train):
+    print(f"[DEBUG] Invoked process_adwin_batch for {'train' if is_train else 'test'} stream, Epoch: {epoch_id}")
     try:
-        print(f"[DEBUG] Epoch {epoch_id}: Converting Spark DataFrame to Pandas DataFrame...")
-        df.printSchema()
-        df.show(5, truncate=False)
-        pdf = df.toPandas()  #conversion to pandas dataframe
-        print(f"[DEBUG] Data types in Pandas DataFrame:\n{pdf.dtypes}")
-        print(f"[DEBUG] Sample rows from Pandas DataFrame:\n{pdf.head()}")
-        if "timestamp" in pdf.columns:
-            pdf["timestamp"] = pdf["timestamp"].astype(str)
-        if "data_type" in pdf.columns:
-            pdf["data_type"] = pdf["data_type"].astype(str)
+        print(f"[DEBUG] Epoch {epoch_id}: Processing {'train' if is_train else 'test'} batch...")
+        batch_df = df.toPandas()
 
-        print(f"[DEBUG] Epoch {epoch_id}: Pandas DataFrame conversion complete. Number of rows: {len(pdf)}")
-        
-        db_manager.bulk_insert(pdf)  #insert into db
-        print(f"[DEBUG] Epoch {epoch_id}: Bulk insert complete. {len(pdf)} rows written to the database.")
+        if batch_df.empty:
+            print(f"[INFO] Epoch {epoch_id}: Empty batch, skipping.")
+            return
+
+        batch_df["drift_detected"] = 0
+        for index, row in batch_df.iterrows():
+            value = row["P1_FCV01D"]
+
+            if pd.isna(value):
+                print(f"[WARNING] Skipping invalid value at index {index}")
+                continue
+            adwin.update(value)
+            batch_df.at[index, "drift_detected"] = int(adwin.drift_detected)
+
+        db_manager = train_db if is_train else test_db
+        print(f"[DEBUG] Batch to insert (epoch {epoch_id}):\n{batch_df}")
+        db_manager.bulk_insert(batch_df)
+        print(f"[DEBUG] Epoch {epoch_id}: Batch results written to database.")
+
     except Exception as e:
-        print(f"[ERROR] Epoch {epoch_id}: An error occurred while writing to the database: {e}")
-        raise e
+        print(f"[ERROR] Epoch {epoch_id}: Error processing batch: {e}")
 
-#write train into database
-print("[DEBUG] Starting writeStream for train stream...")
+
+
+
+print("DEBUG: INVOKE ADWIN FOR TRAIN")
 train_stream.writeStream \
-    .foreachBatch(lambda df, epoch_id: write_to_database(df, epoch_id, train_db)) \
+    .foreachBatch(lambda df, epoch_id: process_adwin_batch(df, epoch_id, is_train=True)) \
+    .option("checkpointLocation", "checkpoints/train_checkpoint") \
     .start()
 
-#write test into database
-print("[DEBUG] Starting writeStream for test stream...")
+print( "DEBUG: INVOKE ADWIN FOR TEST")
 test_stream.writeStream \
-    .foreachBatch(lambda df, epoch_id: write_to_database(df, epoch_id, test_db)) \
+    .foreachBatch(lambda df, epoch_id: process_adwin_batch(df, epoch_id, is_train=False)) \
+    .option("checkpointLocation", "checkpoints/test_checkpoint") \
     .start()
 
-spark.streams.awaitAnyTermination()
+# def write_to_database(df, epoch_id, db_manager):
+#     """Write micro-batch to database."""
+#     try:
+#         print(f"[DEBUG] Epoch {epoch_id}: Converting Spark DataFrame to Pandas DataFrame...")
+#         df.printSchema()
+#         df.show(5, truncate=False)
+#         pdf = df.toPandas()  #conversion to pandas dataframe
+#         print(f"[DEBUG] Data types in Pandas DataFrame:\n{pdf.dtypes}")
+#         print(f"[DEBUG] Sample rows from Pandas DataFrame:\n{pdf.head()}")
+#         if "timestamp" in pdf.columns:
+#             pdf["timestamp"] = pdf["timestamp"].astype(str)
+#         if "data_type" in pdf.columns:
+#             pdf["data_type"] = pdf["data_type"].astype(str)
+
+#         print(f"[DEBUG] Epoch {epoch_id}: Pandas DataFrame conversion complete. Number of rows: {len(pdf)}")
+        
+#         db_manager.bulk_insert(pdf)  #insert into db
+#         print(f"[DEBUG] Epoch {epoch_id}: Bulk insert complete. {len(pdf)} rows written to the database.")
+#     except Exception as e:
+#         print(f"[ERROR] Epoch {epoch_id}: An error occurred while writing to the database: {e}")
+#         raise e
+
+# #write train into database
+# print("[DEBUG] Starting writeStream for train stream...")
+# train_stream.writeStream \
+#     .foreachBatch(lambda df, epoch_id: write_to_database(df, epoch_id, train_db)) \
+#     .start()
+
+# #write test into database
+# print("[DEBUG] Starting writeStream for test stream...")
+# test_stream.writeStream \
+#     .foreachBatch(lambda df, epoch_id: write_to_database(df, epoch_id, test_db)) \
+#     .start()
+
+try:
+    spark.streams.awaitAnyTermination()
+except KeyboardInterrupt:
+    print("[DEBUG] Terminating Spark Streams...")
+finally:
+    print("[DEBUG] Stopping SparkSession...")
+    spark.stop()
